489f7856e6949e39f6b86b883199894a29bce33a
AzraelBunn <36964375+AzraelBunn@users.noreply.github.com>
2026-07-02 16:56:23 +0200

    Update to 2.1

--- src/data.lua
+++ src/data.lua
@@ -1,7 +1,18 @@
 local dust_ammount = 50
 local molten_ammount = 500
 
-
+local function get_ingredients(dust_name)
+	if settings.startup["evrmd-calcite-required"].value then
+		return {
+				{ type = "item", name = dust_name, amount = dust_ammount },
+				{ type = "item", name = "calcite", amount = 1 }
+			}
+	else
+		return {
+				{ type = "item", name = dust_name, amount = dust_ammount },
+			}
+	end
+end
 
 data:extend({
 {
@@ -13,16 +24,13 @@ data:extend({
 		{icon = "__space-age__/graphics/icons/fluid/molten-iron.png", scale = 0.6, shift = {0, 6}},
 	},
 	icon_size = 64,
-	category = "metallurgy",
+	categories = {"metallurgy"},
 	subgroup = "vulcanus-processes",
 	order = "a[molten]-g[molten-iron]",
 	allow_productivity = true,
 	enabled = false,
 	energy_required = 24,
-	ingredients = {
-		{ type = "item", name = "iron-dust", amount = dust_ammount },
-		{ type = "item", name = "calcite", amount = 1 }
-	},
+	ingredients = get_ingredients("iron-dust"),
 	results = {
 		{ type = "fluid", name = "molten-iron", amount = molten_ammount},
 	}
@@ -36,16 +44,13 @@ data:extend({
 		{icon = "__space-age__/graphics/icons/fluid/molten-copper.png", scale = 0.6, shift = {0, 6}},
 	},
 	icon_size = 64,
-	category = "metallurgy",
+	categories = {"metallurgy"},
 	subgroup = "vulcanus-processes",
 	order = "a[molten]-h[molten-copper]",
 	allow_productivity = true,
 	enabled = false,
 	energy_required = 24,
-	ingredients = {
-		{ type = "item", name = "copper-dust", amount = dust_ammount },
-		{ type = "item", name = "calcite", amount = 1 }
-	},
+	ingredients = get_ingredients("copper-dust"),
 	results = {
 		{ type = "fluid", name = "molten-copper", amount = molten_ammount},
 	}
@@ -69,16 +74,13 @@ if mods["Krastorio2-spaced-out"] then
 		{icon = "__k2so-assets__/icons/fluids/molten-rare-metals.png", scale = 0.6, shift = {0, 6}},
 	},
 	icon_size = 64,
-	category = "metallurgy",
+	categories = {"metallurgy"},
 	subgroup = "vulcanus-processes",
 	order = "a[molten]-i[molten-rare-metals]",
 	allow_productivity = true,
 	enabled = false,
 	energy_required = 24,
-	ingredients = {
-		{ type = "item", name = "rare-metals-dust", amount = dust_ammount },
-		{ type = "item", name = "calcite", amount = 1 }
-	},
+	ingredients = get_ingredients("rare-metals-dust"),
 	results = {
 		{ type = "fluid", name = "kr-molten-rare-metals", amount = molten_ammount},
 	}
---
+++ src/settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+	--Startup settings
+	{
+		type = "bool-setting",
+		name = "evrmd-calcite-required",
+		order = "a-a",
+		setting_type = "startup",
+		default_value = true,
+	}
+})
